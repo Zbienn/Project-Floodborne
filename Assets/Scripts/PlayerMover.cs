71539dcaca5d8a43b424d9bf6d6e8eea7b583542
Leonardo Zacarias <202101132@estudantes.ips.pt>
2025-06-11 19:19:43 +0100

    Menu and one Effect

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,6 +5,8 @@ public class PlayerMover : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform spriteTransform;
+    [SerializeField] private ParticleSystem leftTrailParticles;
+    [SerializeField] private ParticleSystem rightTrailParticles;
 
     private Vector2 movement;
 
@@ -17,6 +19,21 @@ public class PlayerMover : MonoBehaviour
         {
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
             spriteTransform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+
+
+
+            Vector3 particleRotation = new Vector3(0f, 0f, angle + 90f);
+            leftTrailParticles.transform.rotation = Quaternion.Euler(particleRotation);
+            rightTrailParticles.transform.rotation = Quaternion.Euler(particleRotation);
+
+            Debug.Log(particleRotation);
+            if (!leftTrailParticles.isPlaying) leftTrailParticles.Play();
+            if (!rightTrailParticles.isPlaying) rightTrailParticles.Play();
+        }
+        else
+        {
+            if (leftTrailParticles.isPlaying) leftTrailParticles.Stop();
+            if (rightTrailParticles.isPlaying) rightTrailParticles.Stop();
         }
     }
 
